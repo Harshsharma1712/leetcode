ee053ade72ac026023ebb21e9ba8b416528d95b4
Harsh Shahrma <sharmahs017@gmail.com>
2024-08-09 13:44:41 +0530

    add solution and update readme

---
+++ Arrays_String/Q228-summary_ranges.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    let ans = []
+    let i = 0
+
+    while(i < nums.length){
+        let start = nums[i]
+
+        while(i < nums.length - 1 && nums[i] + 1 === nums[i+1]){
+            i++
+        }
+        if(start !== nums[i]){
+            ans.push(start + "->" + nums[i])
+        } else{
+            ans.push(start.toString())
+        }
+        i++
+    }
+    return ans
+};
